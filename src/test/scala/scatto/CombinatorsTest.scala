de69f6eb2abd2d847689b16e932d7b462d95793e
Nimor111 <georgi.bojinov@hotmail.com>
2019-07-05 01:32:07 +0300

    Add more tests for combinators

--- src/test/scala/scatto/CombinatorsTest.scala
+++ src/test/scala/scatto/CombinatorsTest.scala
@@ -1,7 +1,9 @@
 package scatto
 
 import org.scalatest._
-import scatto.parser.Combinators
+import scatto.parser.{ Combinators, Lexer, Monad }
+import scatto.parser.types._
+import scatto.parser.MonadInstances._
 
 class CombinatorsTest extends FlatSpec with Matchers {
   "satisfy" should "parse correct characters" in {
@@ -11,4 +13,67 @@ class CombinatorsTest extends FlatSpec with Matchers {
   it should "fail on incorrect characters" in {
     Combinators.satisfy(x => x == 'a')("b") shouldEqual List()
   }
+
+  "digit" should "parse a digit correctly" in {
+    Combinators.digit("2") shouldEqual List(('2', ""))
+  }
+
+  "lower" should "parse a lowercase letter" in {
+    Combinators.lower("a") shouldEqual List(('a', ""))
+  }
+
+  it should "not parse an uppercase letter" in {
+    Combinators.lower("A") shouldEqual List()
+  }
+
+  "upper" should "parse an uppercase digit" in {
+    Combinators.upper("A") shouldEqual List(('A', ""))
+  }
+
+  it should "not parse a lowercase digit" in {
+    Combinators.upper("a") shouldEqual List()
+  }
+
+  "letter" should "parse upper and lowercase letters" in {
+    Combinators.letter("a") shouldEqual List(('a', ""))
+    Combinators.letter("A") shouldEqual List(('A', ""))
+  }
+
+  "alphanum" should "parse a letter or a digit" in {
+    Combinators.alphanum("1") shouldEqual List(('1', ""))
+    Combinators.alphanum("a") shouldEqual List(('a', ""))
+  }
+
+  "many" should "parse a series of characters" in {
+    Combinators
+      .many(Combinators.digit)("123")
+      .head shouldEqual (List('1', '2', '3'), "")
+    Combinators
+      .many(Combinators.letter)("word")
+      .head shouldEqual (List('w', 'o', 'r', 'd'), "")
+    Combinators
+      .many(Combinators.alphanum)("1w2o3r4d")
+      .head shouldEqual (List('1', 'w', '2', 'o', '3', 'r', '4', 'd'), "")
+  }
+
+  it should "parse an empty sequence" in {
+    Combinators.many(Combinators.letter)("").head shouldEqual (List(), "")
+  }
+
+  "many1" should "not parse an empty sequence" in {
+    Combinators.many1(Combinators.letter)("") shouldEqual List()
+  }
+
+  "sepBy" should "parse a comma separated string of digits" in {
+    Combinators
+      .sepBy(Combinators.digit, Lexer.char(','))("1,2,3")
+      .head shouldEqual (List('1', '2', '3'), "")
+  }
+
+  //  "chainl1" should "parse an expression with plus" in {
+  //    def op(a: Int, b: Int): Int = a + b
+  //    def fp: Parser[(Int, Int) => Int] = Monad[Parser].unit(op)
+  //    Combinators
+  //      .chainl1(Lexer.nat, fp)("1+2+3") shouldEqual 1
+  //  }
 }
